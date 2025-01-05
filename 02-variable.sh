9355aaaa9490cc41c08ce32813ab3491be57c839
Hansh31 <anilchinna13@gmail.com>
2025-01-05 20:13:41 +0530

    new Message

---
+++ 02-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ram
+PERSON2=bheem
+
+
+echo "$PERSON1 : how are you $PERSON2?"
+echo "$PERSON2 : I am good , how about you $PERSON1 ?"
+echo "$PERSON1 : I am good thanks $PERSON2"
