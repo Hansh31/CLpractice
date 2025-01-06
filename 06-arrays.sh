48242436e5d110dee406df828bb2e28de2681b39
Hansh31 <anilchinna13@gmail.com>
2025-01-06 07:04:24 +0530

    new Message

---
+++ 06-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("Pushpa" "rrr" "devara")
+
+echo "first movie name : $MOVIES[0]"
+echo "second movie name : $MOVIES[1]"
+echo "third movie name : $MOVIES[2]"
+
+echo "All movies names : $MOVIES[@]"
