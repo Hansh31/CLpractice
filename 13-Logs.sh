aaa26940f0d738d20c205b1bbcf6bb5f107ccfa4
Hansh31 <anilchinna13@gmail.com>
2025-01-08 03:12:15 +0530

    new Message

---
+++ 13-Logs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="/var/log/clscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+    then  
+        echo -e "$2....$R Failure"
+        exit 1
+    else 
+        echo -e "$2.....$G success"
+    fi  
+}
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute the script"
+    exit 1 #other than 0
+fi
+
+dnf list installed mysql &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then # not installed 
+    dnf install mysql -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installing mysql"
+else 
+     echo -e "MySQL is already ...$Y installed"
+fi
+
+dnf list installed git &>>$LOG_FILE_NAME
+ if [ $? -ne 0 ]
+ then # not installed 
+    dnf install git -y &>>$LOG_FILE_NAME
+    VALIDATE $? "Installing git"
+else 
+     echo -e "Git is already ....... $Y installed"
+fi
