3beea4517d49c7f885192196b66d52571349742a
Hansh31 <anilchinna13@gmail.com>
2025-01-08 00:27:18 +0530

    new Message

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -2,10 +2,12 @@
 
 NUMBER=$1
 
-$ -gt , -lt , -eq , -ge , -le
+# -gt , -lt , -eq , -ge , -le
+
 if [ $NUMBER -gt 100 ]
 then 
      echo "given number is greaterthan 100"
 
 else 
      echo "given number is lessthan 100"
+fi
