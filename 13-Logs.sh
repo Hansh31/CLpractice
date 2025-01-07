82d9dbb0f0d75e6b1357564d076a88728a6019b0
Hansh31 <anilchinna13@gmail.com>
2025-01-08 03:19:28 +0530

    new Message

--- 13-Logs.sh
+++ 13-Logs.sh
@@ -5,6 +5,8 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
+
 LOGS_FOLDER="/var/log/clscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
@@ -13,10 +15,10 @@ LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 VALIDATE(){
    if [ $1 -ne 0 ]
     then  
-        echo -e "$2....$R Failure"
+        echo -e "$2....$R Failure $N"
         exit 1
     else 
-        echo -e "$2.....$G success"
+        echo -e "$2.....$G success$N"
     fi  
 }
 echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
@@ -33,7 +35,7 @@ then # not installed
     dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing mysql"
 else 
-     echo -e "MySQL is already ...$Y installed"
+     echo -e "MySQL is already ...$Y installed $N"
 fi
 
 dnf list installed git &>>$LOG_FILE_NAME
@@ -42,5 +44,5 @@ dnf list installed git &>>$LOG_FILE_NAME
     dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing git"
 else 
-     echo -e "Git is already ....... $Y installed"
+     echo -e "Git is already ....... $Y installed $N"
 fi
