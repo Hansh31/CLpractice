c19e0ab188edfdbb7bebbb2ffc98eb1c212f12ae
Hansh31 <anilchinna13@gmail.com>
2025-01-07 23:46:06 +0530

    new Message

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-MOVIES=("Pusha" "rrr" "Deva")
+MOVIES=("$1" "$2" "$3")
 #index starts from 0 , size is 3
 
-echo "first movie name : ${MOVIES[1]}"
-# echo "second movie name : ${MOVIES[1]}"
-# echo "third movie name : ${MOVIES[2]}"
+echo "first movie name : ${MOVIES[0]}"
+echo "second movie name : ${MOVIES[1]}"
+echo "third movie name : ${MOVIES[2]}"
 
-# echo "All movies names : ${MOVIES[@]}"
+echo "All movies names : ${MOVIES[@]}"
